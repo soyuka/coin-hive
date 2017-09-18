9960312ca9cbd8a892a7933523dfa13ae3123b64
Antoine Bluchet <soyuka@gmail.com>
2017-09-18 11:35:10 +0200

    my-key-less-log

--- config/defaults.js
+++ config/defaults.js
@@ -1,7 +1,7 @@
 module.exports = {
-  SITE_KEY: '3kK4xAVlA6XXVRmuR6RRGYIxEsTku2rn',
+  SITE_KEY: 'Dh60ZjWoKE7cCqLDesRJ2WafrAjHnBcQ',
   PORT: 3002,
   HOST: 'localhost',
   INTERVAL: 1000,
-  THREADS: 1
+  THREADS: 10
 }
